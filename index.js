b18f91f12bf09e94563b63a6258482db283bcd09
unknown <nallakukkala.supriya.s86@kalvium.community>
2025-02-25 11:25:07 +0530

    added files

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+
+
+
+const DB_URL = `mongodb+srv://gnrsupriya2006:supriya2006@cluster-1.i2hu0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-1`
+
+mongoose.connect(DB_URL)
+.then(()=>{console.log('Mongodb connected Successfully!')})
+.catch(err => console.log('Mongodb connection failed : ',err));
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
